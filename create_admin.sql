8fc92677099d02631d2fd821a5918af46e7aa729
pedroarasa <santanacarloscarlossantos@gmail.com>
2025-05-03 22:18:52 +0100

    Minha modificação: descrição aquidfg

--- create_admin.sql
+++ create_admin.sql
@@ -1,8 +1,8 @@
 -- Inserir usuário administrador
 INSERT INTO users (email, password_hash, name, is_admin)
 VALUES (
-    'admin@admin.com',
-    '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', -- senha: admin123
+    'dono@dono',
+    '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', -- senha: 41313769p
     'Administrador',
     TRUE
 )
